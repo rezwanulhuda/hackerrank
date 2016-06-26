61202ec8a8a3d736df9224bf7522c2e9250f15e5
Rezwanul Huda <rhu@bordingvista.com>
2016-06-26 17:40:11 +0600

    Staircase

---
+++ Staircase/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+
+        int n = Convert.ToInt32(Console.ReadLine());
+        int count = 1;
+        while(count <= n)
+        {            
+            Console.WriteLine("".PadRight(count++, '#').PadLeft(n));        
+        }        
+    }
+}
