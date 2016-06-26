ede536f2b2fc6be37179d9bf9ad0a561fb41be39
Rezwanul Huda <rhu@bordingvista.com>
2016-06-26 16:41:42 +0600

    A Very Big Sum

---
+++ A Very Big Sum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        Console.ReadLine();
+        string[] arr_temp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        long sum = 0;
+
+        foreach(int v in arr)
+        {
+            sum += v;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
