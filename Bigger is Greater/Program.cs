441157eaba98becb293722fd795f08264ff3601e
Rezwanul Huda <rhu@bordingvista.com>
2016-09-26 17:04:18 +0600

    Added 2 more problems.

---
+++ Bigger is Greater/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bigger_is_Greater
+{
+    class Solution
+    {
+        static void Main(String[] args)
+        {
+            var cnt = Console.ReadLine();
+
+            var count = int.Parse(cnt);
+            var Result = new List<string>();
+
+            while (count > 0)
+            {
+                var input = Console.ReadLine();
+
+                var inputChars = input.ToCharArray();
+
+                bool found = false;
+                for (int x = inputChars.Length - 1; x > 0; --x)
+                {
+                    if (inputChars[x] > inputChars[x-1])
+                    {
+                        var larger = x;
+
+                        for(var y = x + 1; y < inputChars.Length; ++y)
+                        {
+                            if (inputChars[y] > inputChars[x-1] && inputChars[y] < inputChars[larger])
+                            {
+                                larger = y;
+                            }
+                        }
+
+                        var t = inputChars[x - 1];
+                        inputChars[x - 1] = inputChars[larger];
+                        inputChars[larger] = t;
+
+                        for (var z = 0; z < inputChars.Length - x - 1; ++z)
+                        {
+                            for (var y = x; y < inputChars.Length - z - 1; ++y)
+                            {
+
+
+                                if (inputChars[y] > inputChars[y + 1])
+                                {
+                                    var tmp = inputChars[y];
+                                    inputChars[y] = inputChars[y + 1];
+                                    inputChars[y + 1] = tmp;
+                                }
+
+                            }
+                        }
+                            
+
+                        Result.Add(new string(inputChars));
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    Result.Add("no answer");
+                }
+                --count;
+
+
+            }
+
+            foreach (var r in Result)
+            {
+                Console.WriteLine(r);
+            }
+
+            //Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ Two Strings/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Strings
+{
+    class Solution
+    {
+        static void Main(String[] args)
+        {
+            var cnt = Console.ReadLine();
+
+            var count = int.Parse(cnt);
+            var Result = new List<string>();
+
+            while (count > 0)
+            {
+                var s1 = Console.ReadLine();
+                var s2 = Console.ReadLine();
+
+                bool found = false;
+                for(var c = 'a'; c <= 'z'; ++c)
+                {                    
+                    if (s1.Contains(c.ToString()) && s2.Contains(c.ToString()))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if(found)
+                    Result.Add("YES");
+                else
+                    Result.Add("NO");
+                count--;
+            }
+
+            foreach(var r in Result)
+            {
+                Console.WriteLine(r);
+            }
+            //Console.ReadLine();
+        }
+
+        
+    }
+}
