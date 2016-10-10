62d97a7bb75c8f4781b32bb9ad51c637c1e842ad
Rezwanul Huda <rhu@bordingvista.com>
2016-10-10 17:06:00 +0600

    Added Compare the Triplets

---
+++ Compare the Triplets/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compare_the_Triplets
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            List<int> aliceScore = new List<int>() ;
+            List<int> bobScore = new List<int>();
+            var input = Console.ReadLine().Split(' ');
+            //aliceScore = new int[input.Length];
+            foreach(var num in input)
+            {
+                aliceScore.Add(int.Parse(num));
+            }
+
+            input = Console.ReadLine().Split(' ');
+
+            foreach (var num in input)
+            {
+                bobScore.Add(int.Parse(num));
+            }
+
+            int alice = 0;
+            int bob = 0;
+            for(int x = 0, y = 0; x < aliceScore.Count; ++x, ++y)
+            {
+                if (aliceScore[x] > bobScore[x])
+                {
+                    alice++; continue;
+                }
+                if (bobScore[x] > aliceScore[x])
+                {
+                    bob++; continue;
+                }                    
+            }
+
+            Console.WriteLine(String.Format("{0} {1}", alice, bob));
+
+            //Console.ReadLine();
+
+
+
+        }
+    }
+}
