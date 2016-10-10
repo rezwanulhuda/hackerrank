37211315d0d43640114ebdc5a924c3971b589871
Rezwanul Huda <rhu@bordingvista.com>
2016-10-10 09:52:33 +0600

    Added Flipping bits

---
+++ Flipping bits/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flipping_bits
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            List<uint> answers = new List<uint>();
+            var testCases = int.Parse(Console.ReadLine());
+            uint allOnes = (uint)Math.Pow(2, 32) - 1;
+            while(testCases > 0)
+            {
+                testCases--;
+                var t1 = uint.Parse(Console.ReadLine());
+
+                answers.Add(t1 ^ allOnes);
+
+            }
+
+            foreach(var a in answers)
+            {
+                Console.WriteLine(a);
+            }
+
+            //Console.ReadLine();
+        }
+    }
+}
