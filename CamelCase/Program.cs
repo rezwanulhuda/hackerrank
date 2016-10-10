96afc3ba35ceb085331818c15d8acb0858017529
Rezwanul Huda <rhu@bordingvista.com>
2016-10-10 17:27:50 +0600

    Added camelCase

---
+++ CamelCase/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace CamelCase
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            Console.WriteLine(input.Count(p => p >= 'A' && p <= 'Z') + 1);
+            //Console.ReadLine();
+        }
+    }
+}
