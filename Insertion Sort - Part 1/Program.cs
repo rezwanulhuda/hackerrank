ec5a302902f463ee1cd9bb25cd81969ed6cde7fb
Rezwanul Huda <rhu@bordingvista.com>
2016-10-13 18:28:26 +0600

    added Insertion Sort - Part 1

---
+++ Insertion Sort - Part 1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insertion_Sort___Part_1
+{
+    class Solution
+    {
+        static void print(int[] items)
+        {
+            string output = String.Empty;
+            for(int x = 0; x < items.Length; ++x)
+            {
+                if (x > 0)
+                {
+                    output += " ";
+                }
+                output += items[x];
+            }
+
+            Console.WriteLine(output);
+        }
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            int[] sortedItems = null;
+            int itemToInsert;
+            var items = Console.ReadLine().Split(' ');
+
+            itemToInsert = int.Parse(items[items.Length - 1]);
+            sortedItems = new int[items.Length];
+            for(int y = 0; y < items.Length - 1; ++y)
+            {
+                sortedItems[y] = int.Parse(items[y]);
+            }
+
+            int x = 0;
+            for(; x < sortedItems.Length - 1; ++x)
+            {
+                if (itemToInsert >= sortedItems[x]) continue;
+
+                for (int y = sortedItems.Length - 2; y >= x; --y)
+                {
+                    sortedItems[y + 1] = sortedItems[y];
+                    print(sortedItems);
+                }
+
+                break;
+
+            }
+
+            sortedItems[x] = itemToInsert;
+            print(sortedItems);
+
+            //Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Two Characters/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Characters
+{
+    class Solution
+    {
+        class Distribution
+        {
+            public char Char { get; set; }
+            public int Count { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            var s = Console.ReadLine();
+
+            List<Distribution> dist = new List<Distribution>();
+            //int[] count = new int[26];
+            
+            foreach(char c in s.Distinct())
+            {
+                dist.Add(new Distribution() { Char = c, Count = s.Count(p => p == c) });
+            }
+
+            dist.Sort((p, q) => p.Count - q.Count);
+
+            
+            for(int x = 0; x < dist.Count - 1; ++x)
+            {
+                if ((dist[x].Count - dist[x + 1].Count) != 1)
+                {
+                    s = s.Replace(dist[x].Char.ToString(), String.Empty);
+                    dist.Remove(dist[x]);
+                    x--;
+                }
+            }
+
+            
+            if (dist.Count == 2)
+            {
+
+            }
+
+
+            
+        }
+    }
+}
