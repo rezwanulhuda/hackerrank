bee4108ab36181df6671ea7ab7d0748ae79fc6c9
Rezwanul Huda <rhu@bordingvista.com>
2016-10-13 19:01:03 +0600

    Added Insertion Sort - Part 2

---
+++ Insertion Sort - Part 2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insertion_Sort___Part_2
+{
+    class Solution
+    {
+        static void Print(int[] items)
+        {
+            string output = String.Empty;
+            for(int x = 0; x < items.Length; ++x)
+            {
+                if (x > 0)
+                {
+                    output += " ";
+                }
+                output += items[x];
+            }
+
+            Console.WriteLine(output);
+        }
+
+        static void Insert(int itemToInsert, int itemsInserted, int[] sortedItems)
+        {
+            int x = 0;
+            for (; x < itemsInserted; ++x)
+            {
+                if (itemToInsert >= sortedItems[x]) continue;
+
+                for (int y = itemsInserted - 1; y >= x; --y)
+                {
+                    sortedItems[y + 1] = sortedItems[y];                    
+                }
+
+                break;
+
+            }
+
+            sortedItems[x] = itemToInsert;
+            Print(sortedItems);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            int[] sortedItems = null;
+
+            var items = Console.ReadLine().Split(' ');
+            
+            sortedItems = new int[items.Length];
+            for(int y = 0; y < items.Length; ++y)
+            {
+                sortedItems[y] = int.Parse(items[y]);
+            }
+
+            for(int x = 1; x < sortedItems.Length; ++x)
+            {
+                Insert(sortedItems[x], x, sortedItems);
+            }
+        }
+    }
+}
