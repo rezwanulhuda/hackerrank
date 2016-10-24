f73574af053b11d172ff2531fd83646304bcaadf
Rezwanul Huda <rhu@bordingvista.com>
2016-10-24 09:41:45 +0600

    Added time conversion

---
+++ Time Conversion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Time_Conversion
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            DateTime d = DateTime.ParseExact(input, "hh:mm:sstt", null);
+            Console.WriteLine(d.ToString("HH:mm:ss"));
+            //Console.ReadLine();
+        }
+    }
+}
